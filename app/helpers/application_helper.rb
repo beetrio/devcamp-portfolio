4d0711382e8678f10d327f990c888a15e859e0c1
Daniel Grubbs <daniel@beetrio.net>
2017-10-25 07:24:32 -0500

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,7 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    BeetrioViewTool::Renderer.copyright "Beetrio Networks LLC", "All rights reserved"
+    BeetrioViewTool::Renderer.copyright 'Daniel Grubbs', 'All rights reserved'
   end
 
   def nav_items
@@ -58,17 +58,17 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Daniel Grubbs Portfolio", sticky: false)
+  end
+
 end
-
-
-# Old way of implementing the nav
-#   def nav_helper style, tag_type
-# nav_links = <<NAV
-# <#{tag_type}> <a href="#{root_path}" class="#{style} #{active? root_path}">Home</a>  </#{tag_type}>
-# <#{tag_type}> <a href="#{about_me_path}" class="#{style} #{active? about_me_path}">About</a>  </#{tag_type}>
-# <#{tag_type}> <a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a>  </#{tag_type}>
-# <#{tag_type}> <a href="#{blogs_path}" class="#{style}  #{active? blogs_path}">Blog</a>  </#{tag_type}>
-# <#{tag_type}> <a href="#{portfolios_path}" class="#{style}  #{active? portfolios_path}">Portfolio</a>  </#{tag_type}>
-# NAV
-#     nav_links.html_safe
-#   end
