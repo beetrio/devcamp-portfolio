03a31d964495b0fd6ca67668ae668f54a5f64c52
Daniel Grubbs <daniel@beetrio.net>
2017-11-25 09:37:20 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "daniel@beetrio.net",
+  password: "password1",
+  password_confirmation: "password1",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "password1",
+  password_confirmation: "password1",
+  name: "Regular User"
+)
+
+puts "1 Regular user created"
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
